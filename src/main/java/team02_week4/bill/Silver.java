d5b75cb43e6c225c5de8c9e83ef74c6e7d80db47
Sumin <Sumin@192.168.252.1>
2016-03-31 15:29:46 +0900

    Gold, Silver클래스 구현

--- src/main/java/team02_week4/bill/Silver.java
+++ src/main/java/team02_week4/bill/Silver.java
@@ -1,5 +1,25 @@
 package team02_week4.bill;
 
 public class Silver extends Plan {
+	
+	private double basicRate = 29.95;
+	private int basicMinute = 500;
+	private double additionalLineRate = 21.50;
+	private double ratePerExcessMinute = 0.54;
 
+	public void setBasicRate(double basicRate){
+		this.basicRate = basicRate;
+	}
+	
+	public void setBasicMinute(int basicMinute){
+		this.basicMinute = basicMinute;
+	}
+	
+	public void setAdditionalLineRate(double additionalLineRate){
+		this.additionalLineRate = additionalLineRate;
+	}
+
+	public void setRatePerExcessMinute(double ratePerExcessMinute){
+		this.ratePerExcessMinute = ratePerExcessMinute;
+	}
 }
