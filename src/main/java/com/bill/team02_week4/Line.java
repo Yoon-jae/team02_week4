158156a988efbb8aa8a5388b4db2766f8a245ee1
SuminJegal <geniusry@naver.com>
2016-03-31 21:01:48 +0900

    Line 클래스 메소드와 생성자 구현

--- src/main/java/com/bill/team02_week4/Line.java
+++ src/main/java/com/bill/team02_week4/Line.java
@@ -5,5 +5,34 @@ public class Line {
 	private int usedMinutes;
 	private String phoneNumber;
 	private String lineUserName;
+	
+	public Line(int usedMinutes, String phoneNumber, String lineUserName){
+		this.usedMinutes = usedMinutes;
+		this.phoneNumber = phoneNumber;
+		this.lineUserName = lineUserName;
+	}
+	
+	public int getUsedMinutes(){	
+		return usedMinutes; 
+	}
+	
+	public String getPhoneNumber(){
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(String phoneNumber){
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getLineUserName(){
+		return lineUserName;
+	}
+	
+	public void setLineUserName(String lineUserName){
+		this.lineUserName = lineUserName;
+	}
+	
+	
+	
 
 }
