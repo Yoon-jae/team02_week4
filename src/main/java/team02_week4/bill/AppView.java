943633d52052f0658522725d66e370e6a7488581
starlightcandy <uta9784@gmail.com>
2016-03-31 14:53:28 +0900

    Added scanFile method and constructor

--- src/main/java/team02_week4/bill/AppView.java
+++ src/main/java/team02_week4/bill/AppView.java
@@ -1,5 +1,17 @@
 package team02_week4.bill;
+import java.io.*;
 
 public class AppView {
-
+	private Scanner scan;
+	
+	public AppView(){
+		
+	}
+	
+	private int scanFile(String fileName){
+		return 1;
+	}
+	
+	
 }
+
