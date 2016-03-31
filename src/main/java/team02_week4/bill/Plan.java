a68e21b213e76e5bf9fa40fdd61ace698fc80fba
Sumin <Sumin@192.168.252.1>
2016-03-31 14:47:14 +0900

    add

--- src/main/java/team02_week4/bill/Plan.java
+++ src/main/java/team02_week4/bill/Plan.java
@@ -1,5 +1,14 @@
 package team02_week4.bill;
 
 public class Plan {
+	
+	private double basicRate;
+	private int basicMinute;
+	private double additionalLineRate;
+	private double ratePerExcessMinute;
+	
+	public double getBasicRate(){
+		return 0;
+	}
 
 }
--- src/main/java/team02_week4/bill/RateCalculation.java
+++ src/main/java/team02_week4/bill/RateCalculation.java
@@ -1,5 +1,4 @@
 package team02_week4.bill;
 
-public static calss RateCalculation {
-
+public class RateCalculation{
 }
