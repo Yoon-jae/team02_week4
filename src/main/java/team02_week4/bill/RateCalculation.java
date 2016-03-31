636c5ba41befea15ff694a97809bbe167c8957a6
GeunYoung.Kim <GeunYoung.kim0413@gmail.com>
2016-03-31 15:13:07 +0900

    added some methods

--- src/main/java/team02_week4/bill/RateCalculation.java
+++ src/main/java/team02_week4/bill/RateCalculation.java
@@ -1,5 +1,25 @@
 package team02_week4.bill;
 
-public static calss RateCalculation {
+public static class RateCalculation {
 
+	public static getBasicMonthlyRate(Plan aPlan)
+	{
+		
+	}
+	
+	public static getAdditionaLineRate(Plan aPlan, Line aLine)
+	{
+		
+	}
+	
+	public static getAdditionalMinuteRate(Plan aPlan, Line aLine)
+	{
+		
+	}
+	
+	public static getTotalRate(double a, double b, double c)
+	{
+		
+	}
+	
 }
