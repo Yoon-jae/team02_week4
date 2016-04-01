2d06e9a7e898503215b2bb177cfdb69f4310fab1
SuminJegal <geniusry@naver.com>
2016-04-01 19:10:16 +0900

    print 추가

--- src/main/java/com/bill/team02/week4/FormController.java
+++ src/main/java/com/bill/team02/week4/FormController.java
@@ -13,9 +13,8 @@ public class FormController {
 		System.out.println("		Here is your Bill");
 		System.out.println("-------------------------------------------------");
 		System.out.println("Your Plan 			: " + clientAccountData.getPlan().getPlanName());
-		
-//		System.out.println("Your basic monthly rate		: $" + basicMonthlyRate);
-//		System.out.println("Your additional line rate 	: $" + additionalLineRate);
+		System.out.println("Your basic monthly rate		: $" + clientAccountData.getPlan().getBasicRate());
+		System.out.println("Your total used minutes		: $");
 //		System.out.println("Your additional minuter rate	: $" + additionalMinuterRate);
 		System.out.println("-------------------------------------------------");
 		System.out.println("Your total account rate		: $" + clientAccountData.calculateRate());
