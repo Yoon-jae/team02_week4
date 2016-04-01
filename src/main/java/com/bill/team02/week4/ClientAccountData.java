ec94c93d005748e1fa23d79e29e9679bc525f374
Yoonjae <yoonjae.cho92@gmail.com>
2016-04-01 18:08:36 +0900

    fix tech_debt

--- src/main/java/com/bill/team02/week4/ClientAccountData.java
+++ src/main/java/com/bill/team02/week4/ClientAccountData.java
@@ -1,6 +1,6 @@
 package com.bill.team02.week4;
 
-public class ClientAccountData{
+public class ClientAccountData {
 
 	private Plan plan;
 	private Line[] arrayLine;
@@ -11,48 +11,48 @@ public class ClientAccountData{
 	private final int one = 1;
 	private final int two = 2;
 
-	public ClientAccountData(String[] accountData){
-		this.checkAndSetPlan(accountData[accountData.length-one]);
-		arrayLine = new Line[Integer.parseInt(accountData[accountData.length-two])];
-		for(int i=0; i<accountData.length-two; i++){
-			int newIndex = i/two;
-			if(i%two == zero) {
+	public ClientAccountData(String[] accountData) {
+		this.checkAndSetPlan(accountData[accountData.length - one]);
+		arrayLine = new Line[Integer.parseInt(accountData[accountData.length - two])];
+		for (int i = 0; i < accountData.length - two; i++) {
+			int newIndex = i / two;
+			if (i % two == zero) {
 				arrayLine[newIndex] = new Line(accountData[i]);
 			} else {
 				arrayLine[newIndex].setUsedMinutes(Double.parseDouble(accountData[i]));
 			}
 		}
 	}
-	
-	public String getHostName(){
+
+	public String getHostName() {
 		return hostName;
 	}
 
-	public void setHostName(String hostName){
+	public void setHostName(String hostName) {
 		this.hostName = hostName;
 	}
 
-	public String getEmailAddress(){
+	public String getEmailAddress() {
 		return emailAddress;
 	}
 
-	public void setEmailAddress(String emailAddress){
+	public void setEmailAddress(String emailAddress) {
 		this.emailAddress = emailAddress;
 	}
 
-	public double calculateRate(){
+	public double calculateRate() {
 		double basicMonthlyRate = RateCalculation.getBasicMonthlyRate(plan);
 		double additionalLineRate = RateCalculation.getAdditionalLineRate(plan, arrayLine.length);
 		double additionalMinuteRate = RateCalculation.getAdditionalMinuteRate(plan, arrayLine);
 		return RateCalculation.getTotalRate(basicMonthlyRate, additionalLineRate, additionalMinuteRate);
 	}
 
-	private void checkAndSetPlan(String plan){
-		if("Gold".equals(plan)){
+	private void checkAndSetPlan(String plan) {
+		if ("Gold".equals(plan)) {
 			this.plan = new Gold();
-		} else if("Silver".equals(plan)){
+		} else if ("Silver".equals(plan)) {
 			this.plan = new Silver();
-		} else{
+		} else {
 			System.err.println("There isn't such plan");
 		}
 	}
--- src/main/java/com/bill/team02/week4/FormController.java
+++ src/main/java/com/bill/team02/week4/FormController.java
@@ -3,12 +3,12 @@ package com.bill.team02.week4;
 public class FormController {
 
 	ClientAccountData clientAccountData;
-	
+
 	public FormController(String[] accountData) {
 		clientAccountData = new ClientAccountData(accountData);
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		return "Your total account rate is $" + clientAccountData.calculateRate();
 	}
 }
