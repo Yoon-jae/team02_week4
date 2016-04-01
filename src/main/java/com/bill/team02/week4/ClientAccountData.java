b2ae1a98951a0d8389a0a5a7b12deda5c0f2c7e5
Yoonjae <yoonjae.cho92@gmail.com>
2016-04-01 18:35:45 +0900

    edit

--- src/main/java/com/bill/team02/week4/ClientAccountData.java
+++ src/main/java/com/bill/team02/week4/ClientAccountData.java
@@ -6,17 +6,17 @@ public class ClientAccountData {
 	private Line[] arrayLine;
 	private String hostName;
 	private String emailAddress;
-	
+
 	private static final int NUMBER_OF_LINE_INDEX_ZERO = 0;
 	private static final int NUMBER_OF_LINE_INDEX_ONE = 1;
 	private static final int NUMBER_OF_LINE_INDEX_TWO = 2;
 
-	public ClientAccountData(String[] accountData){
-		this.checkAndSetPlan(accountData[accountData.length-NUMBER_OF_LINE_INDEX_ONE]);
-		arrayLine = new Line[Integer.parseInt(accountData[accountData.length-NUMBER_OF_LINE_INDEX_TWO])];
-		for(int i=0; i<accountData.length-NUMBER_OF_LINE_INDEX_TWO; i++){
-			int newIndex = i/NUMBER_OF_LINE_INDEX_TWO;
-			if(i%NUMBER_OF_LINE_INDEX_TWO == NUMBER_OF_LINE_INDEX_ZERO) {
+	public ClientAccountData(String[] accountData) {
+		this.checkAndSetPlan(accountData[accountData.length - NUMBER_OF_LINE_INDEX_ONE]);
+		arrayLine = new Line[Integer.parseInt(accountData[accountData.length - NUMBER_OF_LINE_INDEX_TWO])];
+		for (int i = 0; i < accountData.length - NUMBER_OF_LINE_INDEX_TWO; i++) {
+			int newIndex = i / NUMBER_OF_LINE_INDEX_TWO;
+			if (i % NUMBER_OF_LINE_INDEX_TWO == NUMBER_OF_LINE_INDEX_ZERO) {
 				arrayLine[newIndex] = new Line(accountData[i]);
 			} else {
 				arrayLine[newIndex].setUsedMinutes(Double.parseDouble(accountData[i]));
@@ -40,7 +40,7 @@ public class ClientAccountData {
 		this.emailAddress = emailAddress;
 	}
 
-	public double calculateRate(){
+	public double calculateRate() {
 		double basicMonthlyRate = RateCalculation.getBasicMonthlyRate(plan);
 		double additionalLineRate = RateCalculation.getAdditionalLineRate(plan, arrayLine.length);
 		double additionalMinuteRate = RateCalculation.getAdditionalMinuteRate(plan, arrayLine);
@@ -53,7 +53,7 @@ public class ClientAccountData {
 		} else if ("Silver".equals(plan)) {
 			this.plan = new Silver();
 		} else {
-			throw new IllegalArgumentException("No such plan"); 
+			throw new IllegalArgumentException("No such plan");
 		}
 	}
 
