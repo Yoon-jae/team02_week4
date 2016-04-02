264db234571272fd3441a0b8970d127a81d9fb51
starlightcandy <uta9784@gmail.com>
2016-04-02 13:13:21 +0900

    Logger issue fixed

--- src/main/java/com/bill/team02/week4/AppController.java
+++ src/main/java/com/bill/team02/week4/AppController.java
@@ -4,19 +4,19 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 import java.util.logging.StreamHandler;
-//import java.util.logging.Handler;
 
 public class AppController {
 
     private AppView appView;
     private FormController formController;
+    private Logger logger = Logger.getLogger("My Logger");
 
     public AppController() {
         this.appView = new AppView();
     }
 
     public void run() {
-        Logger logger = Logger.getLogger("My Logger");
+        
         logger.setUseParentHandlers(false);
         logger.addHandler(new StreamHandler(System.err, new SimpleFormatter()));
         String[] accountData = null;
@@ -24,7 +24,6 @@ public class AppController {
             accountData = this.appView.scanFile();
         } catch (Exception e) {
               logger.log(Level.INFO, "Scan failed", e);
-            //throw new IORuntimeException("Scan failed");
         }
         formController = new FormController(accountData);
         formController.printClientBill();
