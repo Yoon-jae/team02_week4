e0ffa334dc36ecfef751c59a5c5ee5ba3c2aae8e
GeunYoung.Kim <GeunYoung.kim0413@gmail.com>
2016-04-02 11:51:02 +0900

    solve the duplicated error

--- src/main/java/com/bill/team02/week4/Plan.java
+++ src/main/java/com/bill/team02/week4/Plan.java
@@ -1,14 +1,39 @@
 package com.bill.team02.week4;
 
-public abstract class Plan {
+public class Plan {
+    private final double BASIC_RATE;
+    private final int BASIC_MINUTE;
+    private final double ADDITIONAL_LINE_RATE;
+    private final double RATE_PER_EXCESS_MINUTES;
+    private final String PLAN_NAME;
 
-    public abstract double getBasicRate();
+    public Plan(double basic_rate, int basic_minute, double additional_line_rate, double rate_per_excess_minute,
+            String plan_name) {
+        this.BASIC_RATE = basic_rate;
+        this.BASIC_MINUTE = basic_minute;
+        this.ADDITIONAL_LINE_RATE = additional_line_rate;
+        this.RATE_PER_EXCESS_MINUTES = rate_per_excess_minute;
+        this.PLAN_NAME = plan_name;
+    }
 
-    public abstract int getBasicMinute();
+    public double getBasicRate() {
+        return BASIC_RATE;
+    }
 
-    public abstract double getAdditionalLineRate();
+    public int getBasicMinute() {
+        return BASIC_MINUTE;
+    }
 
-    public abstract double getRatePerExcessMinute();
+    public double getAdditionalLineRate() {
+        return ADDITIONAL_LINE_RATE;
+    }
 
-    public abstract String getPlanName();
+    public double getRatePerExcessMinute() {
+        return RATE_PER_EXCESS_MINUTES;
+    }
+
+    public String getPlanName() {
+        return PLAN_NAME;
+    }
+
 }
--- src/main/java/com/bill/team02/week4/Silver.java
+++ src/main/java/com/bill/team02/week4/Silver.java
@@ -2,29 +2,8 @@ package com.bill.team02.week4;
 
 public class Silver extends Plan {
 
-    private static final double BASIC_RATE = 29.95;
-    private static final int BASIC_MINUTE = 500;
-    private static final double ADDITIONAL_LINE_RATE = 21.50;
-    private static final double RATE_PER_EXCESS_MINUTE = 0.54;
-    private static final String PLAN_NAME = "Silver";
-
-    public double getBasicRate() {
-        return BASIC_RATE;
+    public Silver() {
+        super(29.95, 500, 21.50, 0.54, "Silver");
     }
 
-    public int getBasicMinute() {
-        return BASIC_MINUTE;
-    }
-
-    public double getAdditionalLineRate() {
-        return ADDITIONAL_LINE_RATE;
-    }
-
-    public double getRatePerExcessMinute() {
-        return RATE_PER_EXCESS_MINUTE;
-    }
-
-    public String getPlanName() {
-        return PLAN_NAME;
-    }
 }
