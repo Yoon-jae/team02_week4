7e6eb5c3af479cb70dea45b6b912431c0efd77f5
SuminJegal <geniusry@naver.com>
2016-04-02 11:53:09 +0900

    output 출력 수정으로 인한 코드 수정

--- src/main/java/com/bill/team02/week4/FormController.java
+++ src/main/java/com/bill/team02/week4/FormController.java
@@ -11,9 +11,13 @@ public class FormController {
 		System.out.println("=================================================");
 		System.out.println("		Here is your Bill");
 		System.out.println("-------------------------------------------------");
+		System.out.println("              < Your Information >");
 		System.out.println("Your Plan 			: " + clientAccountData.getPlan().getPlanName());
-		System.out.println("Your basic monthly rate		: $" + clientAccountData.getPlan().getBasicRate());
-		System.out.println("Your total used minutes		: $");
+		System.out.println("Number of your member           : " + clientAccountData.getArrayLine().length);
+		System.out.println("Your total used minutes         : " + clientAccountData.getTotalMinutes());
+	    System.out.println("-------------------------------------------------");
+	    System.out.println("           < Rate for used minutes >");
+	    System.out.println("Your basic monthly rate		: $" + clientAccountData.getPlan().getBasicRate());
 //		System.out.println("Your additional minuter rate	: $" + additionalMinuterRate);
 		System.out.println("-------------------------------------------------");
 		System.out.println("Your total account rate		: $" + clientAccountData.calculateRate());
