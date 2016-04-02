5b4c53204d547d4dd8963ad073d7ac8b2ff5a952
starlightcandy <uta9784@gmail.com>
2016-04-02 12:50:43 +0900

    Magic number issue fixed

--- src/main/java/com/bill/team02/week4/Main.java
+++ src/main/java/com/bill/team02/week4/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IORuntimeException {
         AppController appController = new AppController();
         appController.run();
     }
