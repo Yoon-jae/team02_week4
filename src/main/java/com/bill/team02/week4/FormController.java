f787233f5d894a4c8352e75782919139a0d1222b
Yoonjae <yoonjae.cho92@gmail.com>
2016-04-02 12:55:42 +0900

    fix

--- src/main/java/com/bill/team02/week4/FormController.java
+++ src/main/java/com/bill/team02/week4/FormController.java
@@ -2,31 +2,39 @@ package com.bill.team02.week4;
 
 public class FormController {
     private ClientAccountData clientAccountData;
-    private StringBuffer buf;
+    private StringBuilder builder;
 
     public FormController(String[] accountData) {
         clientAccountData = new ClientAccountData(accountData);
-        buf = new StringBuffer();
+        builder = new StringBuilder();
     }
 
     public void printClientBill() {
-        buf.append("=================================================\n");
-        buf.append("               Here is your Bill\n");
-        buf.append("-------------------------------------------------\n");
-        buf.append("              < Your Information >\n");
-        buf.append("Your Plan           : " + clientAccountData.getPlan().getPlanName() + "\n");
-        buf.append("Number of your member           : " + clientAccountData.getArrayLine().length + "\n");
-        buf.append("Your total used minutes         : " + clientAccountData.getTotalMinutes() + "\n");
-        buf.append("-------------------------------------------------\n");
-        buf.append("           < Rate for used minutes >\n");
-        buf.append("Your basic monthly rate         : $" + clientAccountData.getBasicMonthlyRate() + "\n");
-        buf.append("Your additional line rate       : $" + clientAccountData.getAdditionalLineRate() + "\n");
-        buf.append("Your additional minute rate     : $" + clientAccountData.getAdditionalMinuteRate() + "\n");
-        buf.append("          " + clientAccountData.checkFamilyDiscountAndPrint() + "\n");
-        buf.append("-------------------------------------------------\n");
-        buf.append("Your total account rate         : $" + clientAccountData.calculateRate() + "\n");
-        buf.append("=================================================\n");
+        makeOutline();
+        builder.append("               Here is your Bill\n");
+        makeBorderLine();
+        builder.append("              < Your Information >\n");
+        builder.append("Your Plan           : " + clientAccountData.getPlan().getPlanName() + "\n");
+        builder.append("Number of your member           : " + clientAccountData.getArrayLine().length + "\n");
+        builder.append("Your total used minutes         : " + clientAccountData.getTotalMinutes() + "\n");
+        makeBorderLine();
+        builder.append("           < Rate for used minutes >\n");
+        builder.append("Your basic monthly rate         : $" + clientAccountData.getBasicMonthlyRate() + "\n");
+        builder.append("Your additional line rate       : $" + clientAccountData.getAdditionalLineRate() + "\n");
+        builder.append("Your additional minute rate     : $" + clientAccountData.getAdditionalMinuteRate() + "\n");
+        builder.append("          " + clientAccountData.checkFamilyDiscountAndPrint() + "\n");
+        makeBorderLine();
+        builder.append("Your total account rate         : $" + clientAccountData.calculateRate() + "\n");
+        makeOutline();
 
-        System.out.println(buf);
+        System.out.println(builder);
+    }
+
+    private void makeOutline() {
+        builder.append("=================================================\n");
+    }
+
+    private void makeBorderLine() {
+        builder.append("-------------------------------------------------\n");
     }
 }
