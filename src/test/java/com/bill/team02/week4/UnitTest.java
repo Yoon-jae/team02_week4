f62d72a3c9bc81347b1afc9e8fe850383dfe798a
Yoonjae <yoonjae.cho92@gmail.com>
2016-04-12 14:47:58 +0900

    add Jtest

--- src/test/java/com/bill/team02/week4/UnitTest.java
+++ src/test/java/com/bill/team02/week4/UnitTest.java
@@ -8,34 +8,33 @@ public class UnitTest {
 
     @Test
     public void testAppController() {
-        assertTrue( true );
-        com.bill.team02.week4.Main.main(null);
+        assertTrue(true);
     }
 
     @Test
     public void testRun() {
-        Main main = new Main();
-        RateCalculation rCalc = new RateCalculation();
+        new Main();
+        Main.main(null);
     }
-    
+
     @Test
     public void runFirstFile() {
         AppController app = new AppController();
         app.run("input_PersonalInfo.txt");
     }
-    
+
     @Test
     public void runSecondFile() {
         AppController app = new AppController();
         app.run("input_PersonalInfo2.txt");
     }
-    
+
     @Test
     public void runThirdFile() {
         AppController app = new AppController();
         app.run("input_PersonalInfo3.txt");
     }
-    
+
     @Test
     public void runErrorFile() {
         AppController app = new AppController();
