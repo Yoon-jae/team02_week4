e60a87020ec18cb8eea49090e9df3c0ed858da7c
starlightcandy <uta9784@gmail.com>
2016-04-12 21:31:41 +0900

    Typing error fixed

--- src/test/java/com/bill/team02/week4/RateCalculationTest.java
+++ src/test/java/com/bill/team02/week4/RateCalculationTest.java
@@ -10,8 +10,6 @@ public class RateCalculationTest {
         getAdditionalLineRateTest();
         getAdditionalMinuteRateTest();
         getTotalRateTest();
-        
-        s
     }
     
     @Test
