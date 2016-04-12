99660c51e40dbe37ad437b8c0887c5ae91d85059
Yoonjae <yoonjae.cho92@gmail.com>
2016-04-12 15:56:31 +0900

    add Jtest

--- src/test/java/com/bill/team02/week4/UnitTest.java
+++ src/test/java/com/bill/team02/week4/UnitTest.java
@@ -2,50 +2,14 @@ package com.bill.team02.week4;
 
 import static org.junit.Assert.*;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Modifier;
-
 import org.junit.Test;
 
 public class UnitTest {
 
     @Test
-    public void testAppController() {
-        com.bill.team02.week4.Main.main(null);
+    public void test() {
+        Main.main(null);
         assertTrue(true);
-    }
-
-    @Test
-    public void testConstructorIsPrivate() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-      Constructor<Main> constructor = Main.class.getDeclaredConstructor();
-      assertTrue(Modifier.isPrivate(constructor.getModifiers()));
-      constructor.setAccessible(true);
-      constructor.newInstance();
-    }
-
-    @Test
-    public void runFirstFile() {
-        AppController app = new AppController();
-        app.run("input_PersonalInfo.txt");
-    }
-
-    @Test
-    public void runSecondFile() {
-        AppController app = new AppController();
-        app.run("input_PersonalInfo2.txt");
-    }
-
-    @Test
-    public void runThirdFile() {
-        AppController app = new AppController();
-        app.run("input_PersonalInfo3.txt");
-    }
-
-    @Test
-    public void runErrorFile() {
-        AppController app = new AppController();
-        app.run("Error_fileName.txt");
     }
 
 }
