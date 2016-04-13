10bed258cdec4b28e64612f944976fc35e472e43
Yoonjae <yoonjae.cho92@gmail.com>
2016-04-13 12:18:05 +0900

    silver, plan unit test

--- src/test/java/com/bill/team02/week4/UnitTest.java
+++ src/test/java/com/bill/team02/week4/UnitTest.java
@@ -28,8 +28,22 @@ public class UnitTest {
     
     @Test
     public void testPlan() {
-        
-        com.bill.team02.week4.Main.main(null);
+        Plan plan = new Plan(100.0, 1000, 200.0, 150.0, "planName");
+        assertEquals(100.0, plan.getBasicRate(),30);
+        assertEquals(1000, plan.getBasicMinute(),30);
+        assertEquals(200.0, plan.getAdditionLineRate(),30);
+        assertEquals(150.0, plan.getRatePerExcessMinute(),30);
+        assertEquals("planName", plan.getPlanName());
+    }
+    
+    @Test
+    public void testSilver() {
+        Plan silver = new Silver();
+        assertEquals(29.95, silver.getBasicRate(),30); 
+        assertEquals(500, silver.getBasicMinute(),30); 
+        assertEquals(21.50, silver.getAdditionLineRate(),30); 
+        assertEquals(0.54, silver.getRatePerExcessMinute(),30); 
+        assertEquals("Silver", silver.getPlanName()); 
     }
 
 }
