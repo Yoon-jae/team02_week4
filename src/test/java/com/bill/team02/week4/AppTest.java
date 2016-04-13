3fa5c19aec530d9ea28cf8846098523aabcf8a9e
GeunYoung.Kim <GeunYoung.kim0413@gmail.com>
2016-04-13 11:07:56 +0900

    d

--- src/test/java/com/bill/team02/week4/AppTest.java
+++ src/test/java/com/bill/team02/week4/AppTest.java
@@ -9,9 +9,63 @@ import org.junit.Test;
 public class AppTest {
     
     @Test
-    public void testGoldApp(){
-        Main.main(new String[]{"input_PersonalInfo"});
-        assertEquals(true, true);
+    public void getBasicMonthlyRateTest() {
+        Gold g = new Gold();
+        Silver s = new Silver();
+
+        RateCalculation c = new RateCalculation();
+
+        assertEquals(g.getBasicRate(), c.getBasicMonthlyRate(g),0.05);
+        assertEquals(s.getBasicRate(), c.getBasicMonthlyRate(s),0.05);
+
+    }
+
+    @Test
+    public void getAdditionalLineRateTest() {
+        Gold g = new Gold();
+        Silver s = new Silver();
+
+        RateCalculation r = new RateCalculation();
+
+        for (int aNumberOfLine = 0; aNumberOfLine < 15; aNumberOfLine++) {
+            r.getAdditionalLineRate(g, aNumberOfLine);
+            r.getAdditionalLineRate(s, aNumberOfLine);
+        }
+    }
+    
+    @Test
+    public void getAdditionalMinuteRateTest() throws IOException{
+        Gold g = new Gold();
+        Silver s = new Silver();
+
+       
+        Line[] l = new ClientAccountData(new AppView().scanFile("input_PersonalInfo.txt")).getArrayLine();
+
+        RateCalculation r = new RateCalculation();
+        
+        r.getAdditionalMinuteRate(g,l);
+        r.getAdditionalMinuteRate(s,l);
+
+    }
+
+    @Test
+    public void getTotalRateTest() throws IOException {
+
+        Gold g = new Gold();
+        Silver s = new Silver();
+
+        RateCalculation c = new RateCalculation();
+        Line[] l = new ClientAccountData(new AppView().scanFile("input_PersonalInfo.txt")).getArrayLine();
+
+        for (int aNumberOfLine = 0; aNumberOfLine < 15; aNumberOfLine++) {
+        c.getTotalRate(c.getBasicMonthlyRate(g), c.getAdditionalLineRate(g, aNumberOfLine), c.getAdditionalMinuteRate(g,l));
+        c.getTotalRate(c.getBasicMonthlyRate(s), c.getAdditionalLineRate(s, aNumberOfLine), c.getAdditionalMinuteRate(s,l));
+        }
+    }
+    
+    @Test
+    public void testApp() {
+        Main.main(null);
     }
     
 }
