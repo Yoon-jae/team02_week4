a0818db620e6ddbcb9fb34e281a955769117f9e1
starlightcandy <uta9784@gmail.com>
2016-04-14 16:13:23 +0900

    Unused org.junit.before removed.

--- src/test/java/com/bill/team02/week4/MainTest.java
+++ src/test/java/com/bill/team02/week4/MainTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class MainTest {
