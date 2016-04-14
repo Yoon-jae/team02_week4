185a824ac99e136e1f097eae64751abcc5736b45
starlightcandy <uta9784@gmail.com>
2016-04-14 16:10:25 +0900

    Restore private constructor test method

--- src/test/java/com/bill/team02/week4/MainTest.java
+++ src/test/java/com/bill/team02/week4/MainTest.java
@@ -1,13 +1,28 @@
 package com.bill.team02.week4;
 
 import static org.junit.Assert.*;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+
+import org.junit.Before;
 import org.junit.Test;
+
 public class MainTest {
+    
+    @Test
+    public void testConstructorIsPrivate() throws Exception {
+      Constructor<Main> constructor = Main.class.getDeclaredConstructor();
+      assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+      constructor.setAccessible(true);
+      constructor.newInstance();
+    }
 
     @Test
     public void testAppController() {
         Main.main(null);
-        assertTrue(true);
+        assertTrue( true );
     }
-
+    
+    
 }
