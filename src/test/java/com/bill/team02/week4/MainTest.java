98563f193df41ba6239078f9ec3a485d4993b073
starlightcandy <uta9784@gmail.com>
2016-04-14 15:26:35 +0900

    Rename UnitTest to MainTest, testConstructorIsPrivate moved to @Before

--- src/test/java/com/bill/team02/week4/MainTest.java
+++ src/test/java/com/bill/team02/week4/MainTest.java
@@ -5,23 +5,24 @@ import static org.junit.Assert.*;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class UnitTest {
-
-    @Test
-    public void testAppController() {
-        Main.main(null);
-        assertTrue( true );
-    }
-/*
-    @Test
+    
+    @Before
     public void testConstructorIsPrivate() throws Exception {
       Constructor<Main> constructor = Main.class.getDeclaredConstructor();
       assertTrue(Modifier.isPrivate(constructor.getModifiers()));
       constructor.setAccessible(true);
       constructor.newInstance();
     }
-  */  
+
+    @Test
+    public void testAppController() {
+        Main.main(null);
+        assertTrue( true );
+    }
+    
     
 }
