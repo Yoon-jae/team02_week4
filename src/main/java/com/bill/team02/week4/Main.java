fe2e30cc032abd3c4a7a8e8713da7b332ae13cb0
starlightcandy <uta9784@gmail.com>
2016-04-14 16:00:32 +0900

    Restored private main constructor

--- src/main/java/com/bill/team02/week4/Main.java
+++ src/main/java/com/bill/team02/week4/Main.java
@@ -2,6 +2,10 @@ package com.bill.team02.week4;
 
 public class Main {
 
+    private Main() {
+        // Intentionally Empty
+    }
+
     public static void main(String[] args) {
         AppController appController = new AppController();
         appController.run("input_PersonalInfo.txt");
