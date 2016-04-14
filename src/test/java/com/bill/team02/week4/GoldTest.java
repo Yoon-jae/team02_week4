3585adf61e6267e41ac4957bf0832e32711f501c
starlightcandy <uta9784@gmail.com>
2016-04-14 18:39:11 +0900

    limit time fixed.

--- src/test/java/com/bill/team02/week4/GoldTest.java
+++ src/test/java/com/bill/team02/week4/GoldTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 public class GoldTest {
     private Gold gold;
+    private static final double TEST_LIMIT = 0.05;
     
     @Before
     public void setUp(){
@@ -13,22 +14,22 @@ public class GoldTest {
     }
     @Test
     public void testGoldBasicRate() {
-        assertEquals(49.95, gold.getBasicRate(),30); 
+        assertEquals(49.95, gold.getBasicRate(),TEST_LIMIT); 
     }
     
     @Test
     public void testGoldBasicMinute() {
-        assertEquals(1000, gold.getBasicMinute(),30); 
+        assertEquals(1000, gold.getBasicMinute(),TEST_LIMIT); 
     }
     
     @Test
     public void testGoldAdditionLineRate() {
-        assertEquals(14.50, gold.getAdditionLineRate(),30); 
+        assertEquals(14.50, gold.getAdditionLineRate(),TEST_LIMIT); 
     }
     
     @Test
     public void testGoldRatePerExcessMinute() {
-        assertEquals(0.45, gold.getRatePerExcessMinute(),30); 
+        assertEquals(0.45, gold.getRatePerExcessMinute(),TEST_LIMIT); 
     }
     
     @Test
