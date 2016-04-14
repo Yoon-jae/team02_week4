98e7f785f86a9f487c4d99b3ff039c184c310b71
starlightcandy <uta9784@gmail.com>
2016-04-14 15:38:17 +0900

    If statement fixed.

--- src/main/java/com/bill/team02/week4/AppView.java
+++ src/main/java/com/bill/team02/week4/AppView.java
@@ -9,7 +9,7 @@ public class AppView {
     private static final int MUL_PAIR = 2;
     private static final int ADD_PAIR = 1;
     private static final int LINE_NUMBER_ONE = 1;
-    private static final int LINE_NUMBER_TWO = 2;
+    //private static final int LINE_NUMBER_TWO = 2;
     private static final int MAX_DATA_SIZE = 1000;
 
     public AppView() {
@@ -27,18 +27,16 @@ public class AppView {
         String planName = null;
         while (token.nextToken() != StreamTokenizer.TT_EOF) {
             if (token.ttype == StreamTokenizer.TT_NUMBER) {
-                if (token.lineno() == LINE_NUMBER_TWO) {
+                //if (token.lineno() == LINE_NUMBER_TWO) {
                     int numOfLine = (int) token.nval;
                     accountData = new String[(numOfLine + ADD_PAIR) * MUL_PAIR];
                     accountData[accountData.length - PLAN_IDNEX] = planName;
                     accountData[accountData.length - NUM_OF_LINE_INDEX] = String.valueOf(numOfLine);
-                }
-                else{
-                    
-                }
+                //}
+               
                 continue;
-            }
-            if (token.ttype == StreamTokenizer.TT_WORD) {
+            } else// (token.ttype == StreamTokenizer.TT_WORD)
+                {
                 if (token.lineno() == LINE_NUMBER_ONE) {
                     planName = token.sval.toLowerCase();
                 } else {
@@ -48,9 +46,6 @@ public class AppView {
                     String usedMinute = Double.toString(token.nval);
                     accountData[accountDataIndex++] = usedMinute;
                 }
-            }
-            else{
-                
             }
         }
         stream.close();
