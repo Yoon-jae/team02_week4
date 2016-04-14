f51f610fec1c24d93c85fedcc625133d748971e4
starlightcandy <uta9784@gmail.com>
2016-04-14 15:44:06 +0900

    Fixing exception issue

--- src/test/java/com/bill/team02/week4/MainTest.java
+++ src/test/java/com/bill/team02/week4/MainTest.java
@@ -3,26 +3,49 @@ package com.bill.team02.week4;
 import static org.junit.Assert.*;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class MainTest {
-    
+
     @Before
-    public void testConstructorIsPrivate() throws Exception {
-      Constructor<Main> constructor = Main.class.getDeclaredConstructor();
-      assertTrue(Modifier.isPrivate(constructor.getModifiers()));
-      constructor.setAccessible(true);
-      constructor.newInstance();
+    public void testConstructorIsPrivate() {
+        Constructor<Main> constructor = null;
+        try {
+            constructor = Main.class.getDeclaredConstructor();
+        } catch (NoSuchMethodException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+        constructor.setAccessible(true);
+        try {
+            constructor.newInstance();
+        } catch (InstantiationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void testAppController() {
         Main.main(null);
-        assertTrue( true );
+        assertTrue(true);
     }
-    
-    
+
 }
