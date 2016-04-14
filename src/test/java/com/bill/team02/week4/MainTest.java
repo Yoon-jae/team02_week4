1347dddfa06e9e8d9bb391153b707e71361b3210
starlightcandy <uta9784@gmail.com>
2016-04-14 18:29:50 +0900

    private constructor test removed

--- src/test/java/com/bill/team02/week4/MainTest.java
+++ src/test/java/com/bill/team02/week4/MainTest.java
@@ -5,13 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MainTest {
-    
-    @Test(expected=IllegalAccessException.class)
-    public void testConstructorIsPrivate() throws Exception{
-        Main.class.newInstance();
-        fail("This calss constructor should be private");
-    }
-
     @Test
     public void testAppController() {
         Main.main(null);
